875c4e8a8930499b2cbe9a5990d975f31bd36717
kkura <flkuratomijr@gmail.com>
2026-05-12 11:22:57 -0300

    mentalizando classes iniciais

---
+++ src/br/com/fiap/CalculadoraO2/models/PontuacaoUsuario.java
@@ -0,0 +1,4 @@
+package br.com.fiap.CalculadoraO2.models;
+
+public class PontuacaoUsuario extends Usuario{
+}
---
+++ src/br/com/fiap/CalculadoraO2/models/RegistrarAcao.java
@@ -0,0 +1,6 @@
+package br.com.fiap.CalculadoraO2.models;
+
+public class RegistrarAcao {
+
+    // puxa a classe de calculadora e acao sustentavel
+}
