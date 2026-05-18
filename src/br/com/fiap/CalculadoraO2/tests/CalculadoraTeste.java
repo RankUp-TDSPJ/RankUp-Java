7a2d9c1ddeecc344d454ee6f89e50e1b7da06b39
flavio <rm571211@fiap.com.br>
2026-05-18 08:43:13 -0300

    created main

--- src/br/com/fiap/CalculadoraO2/tests/CalculadoraTeste.java
+++ src/br/com/fiap/CalculadoraO2/tests/CalculadoraTeste.java
@@ -1,4 +1,15 @@
 package br.com.fiap.CalculadoraO2.tests;
 
+import br.com.fiap.CalculadoraO2.models.AcaoSustentavel;
+
+import java.util.Scanner;
+
 public class CalculadoraTeste {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        AcaoSustentavel acaoSustentavel = new AcaoSustentavel();
+
+    }
 }
