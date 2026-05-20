57fe968e4a42cb4be4ae613e2246f8a1a447892a
kkura <flkuratomijr@gmail.com>
2026-05-20 00:01:22 -0300

    usuario acredito que feita

--- src/br/com/fiap/CalculadoraO2/models/Usuario.java
+++ src/br/com/fiap/CalculadoraO2/models/Usuario.java
@@ -9,16 +9,59 @@ public class Usuario implements Exibivel {
     private int idade;
     private PontuacaoUsuario pontuacao;
 
+    public Usuario (String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.pontuacao = null;
+
+    }
+
     public void exibirPerfil() {
+        System.out.println("==== Perfil do Usuario ====");
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Idade: " + idade);
+        if (pontuacao != null) {
+            pontuacao.exibirPontuacao();
+        } else {
+            System.out.println("Nenhuma acao registrada ainda.");
+        }
 
     }
 
     @Override
     public void exibir() {
-
+        exibirPerfil();
     }
+
+
 
     public String getNome() {
         return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public PontuacaoUsuario getPontuacao() {
+        return pontuacao;
+    }
+    public void setPontuacao(PontuacaoUsuario pontuacao) {
+        this.pontuacao = pontuacao;
     }
 }
